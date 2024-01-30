135d0647974d102263ba87421b019b033a268365
Khan Faiz <khan.faiz.ayub@gmail.com>
2024-01-31 01:55:17 +0530

    Asynchronous JS Async / Await

--- starter/script.js
+++ starter/script.js
@@ -330,9 +330,24 @@ bookAfterUpdate;
 */
 
 // Asynchronous JS Promises
-
+/*
 fetch("https://jsonplaceholder.typicode.com/todos")
   .then((res) => res.json())
   .then((data) => console.log(data));
+
+console.log("Mr.Khan");
+*/
+
+// Asynchronous JS Async / Await
+
+async function getTodos() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+  const data = await res.json();
+  console.log(data);
+  return data;
+}
+
+const todos = getTodos();
+console.log(todos);
 
 console.log("Mr.Khan");
